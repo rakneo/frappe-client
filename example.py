8c936d720019dc178c011a98c01e9fc4acb9bf18
Rakshith Vikramaj <rv.g6598@outlook.com>
2021-12-01 17:01:54 +0530

    Setup Configures

--- example.py
+++ example.py
@@ -12,40 +12,42 @@ LIKES = 6
 LINKS = 7
 PHONE = 8
 
+
 def sync():
-	print("logging in...")
-	client = FrappeClient("https://xxx.frappecloud.com", "xxx", "xxx")
+  print("logging in...")
+  client = FrappeClient("https://xxx.frappecloud.com", "xxx", "xxx")
 
-	with open("jobs.csv", "rU") as jobsfile:
-		reader = csv.reader(jobsfile, dialect='excel')
-		for row in reader:
-			if row[0]=="Timestamp":
-				continue
+  with open("jobs.csv", "rU") as jobsfile:
+    reader = csv.reader(jobsfile, dialect='excel')
+    for row in reader:
+      if row[0] == "Timestamp":
+        continue
 
-			print("finding " + row[EMAIL])
-			name = client.get_value("Job Applicant", "name", {"email_id": row[EMAIL]})
+      print("finding " + row[EMAIL])
+      name = client.get_value("Job Applicant", "name", {"email_id": row[EMAIL]})
 
-			if name:
-				doc = client.get_doc("Job Applicant", name["name"])
-			else:
-				doc = {"doctype":"Job Applicant"}
+      if name:
+        doc = client.get_doc("Job Applicant", name["name"])
+      else:
+        doc = {"doctype": "Job Applicant"}
 
-			doc["applicant_name"] = row[NAME]
-			doc["email_id"] = row[EMAIL]
-			doc["introduction"] = row[INTRODUCTION]
-			doc["thoughts_on_company"] = row[THOUGHTS_ON_COMPANY]
-			doc["likes"] = row[LIKES]
-			doc["links"] = row[LINKS]
-			doc["phone_number"] = row[PHONE]
-			if doc.get("status") != "Rejected":
-				doc["status"] = "Filled Form"
+      doc["applicant_name"] = row[NAME]
+      doc["email_id"] = row[EMAIL]
+      doc["introduction"] = row[INTRODUCTION]
+      doc["thoughts_on_company"] = row[THOUGHTS_ON_COMPANY]
+      doc["likes"] = row[LIKES]
+      doc["links"] = row[LINKS]
+      doc["phone_number"] = row[PHONE]
+      if doc.get("status") != "Rejected":
+        doc["status"] = "Filled Form"
 
-			if name:
-				client.update(doc)
-				print("Updated " + row[EMAIL])
-			else:
-				client.insert(doc)
-				print("Inserted " + row[EMAIL])
+      if name:
+        client.update(doc)
+        print("Updated " + row[EMAIL])
+      else:
+        client.insert(doc)
+        print("Inserted " + row[EMAIL])
 
-if __name__=="__main__":
-	sync()
+
+if __name__ == "__main__":
+  sync()
--- setup.py
+++ setup.py
@@ -6,16 +6,30 @@ with open('requirements.txt') as requirements:
     install_requires = requirements.read().split()
 
 setup(
-    name='frappeclient',
+    name='frappe-client',
+    packages=['frappeclient'],
     version=version,
-    author='Rushabh Mehta',
-    author_email='rushabh@erpnext.com',
-    packages=[
-        'frappeclient'
-    ],
+    license='MIT',
+    author='Rakshith Vikramraj',
+    author_email='rakshithvikramraj@gmail.com',
+    url="https://github.com/rakneo/frappe-client",
+    download_url="https://github.com/rakneo/frappe-client/archive/refs/tags/v1.0.tar.gz",
+    keywords=['frappe', 'erpnext', 'frappe-rest'],
     install_requires=install_requires,
     tests_requires=[
         'httmock<=1.2.2',
         'nose<=1.3.4'
     ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+
+      ],
 )
